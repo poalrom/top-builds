bfe112350fbb44b8d64c55bc24a7f433278aee03
Alex Popkov <poalrom@yandex.ru>
2020-04-01 18:48:44 +0300

    Add slots to essences in cache

--- scripts/mappers/getEssencesFromEquipment.ts
+++ scripts/mappers/getEssencesFromEquipment.ts
@@ -1,15 +1,27 @@
 import { ICharacterEquip } from "../interfaces/ICharacterEquip";
+import { PartialEmpty } from "../errors/PartialEmpty";
 
 export function getEssencesFromEquipment(equipment: ICharacterEquip) {
     const heartOfAzeroth = equipment.equipped_items
         .find(item => item.hasOwnProperty("azerite_details") && item.azerite_details.hasOwnProperty("selected_essences"));
 
-    return !heartOfAzeroth ? [] : heartOfAzeroth.azerite_details.selected_essences
+    if (!heartOfAzeroth) {
+        throw new PartialEmpty("essences");
+    }
+
+    return heartOfAzeroth.azerite_details.selected_essences
         .map((essence) => {
             const spellTooltip = essence.slot === 0 ?
                 essence.main_spell_tooltip :
                 essence.passive_spell_tooltip;
 
-            return spellTooltip && spellTooltip.spell && spellTooltip.spell.id ? spellTooltip.spell.id : undefined;
-        }).filter(Boolean);
+            if (!spellTooltip || !spellTooltip.spell || !spellTooltip.spell.id) {
+                throw new PartialEmpty("essences");
+            }
+
+            return {
+                id: spellTooltip.spell.id,
+                slot: essence.slot > 0 ? "SMALL" : "BIG",
+            };
+        });
 }
