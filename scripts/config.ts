37d231cda9255ce3752bc01c3e38877ac3d8dfef
Alex Popkov <poalrom@yandex.ru>
2020-04-11 11:49:36 +0300

    Add limit to cache update

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,7 @@
 export const config = {
     BNET_ID: process.env.BNET_ID || "",
     BNET_SECRET: process.env.BNET_SECRET || "",
+    forceCache: !!process.env.FORCE_CACHE,
     season: process.env.SEASON || "",
     classSpecRange: {
         from: Number(process.env.CACHE_FROM) || 0,
--- scripts/index.ts
+++ scripts/index.ts
@@ -9,12 +9,31 @@ import { config } from "./config";
 import { getCharactersInfo } from "./controllers/getCharactersInfo";
 
 const promisifiedWriteFile = promisify(fs.writeFile);
+// Minimum cache interval in hours
+const minCacheInterval = 12;
 
 async function run() {
-    await promisifiedWriteFile(
-        path.resolve(__dirname, "../public/data/classes.json"),
-        JSON.stringify(ClassesWithSpecs)
+    const metaFilePath = path.resolve(__dirname, "../public/data/meta.json");
+    const metaData = JSON.parse(fs.readFileSync(metaFilePath).toString());
+    // Cache freshness in hours
+    const cacheFreshness = Math.ceil(
+        (Date.now() - metaData.lastCache) /
+        // ms    s    m
+        (1000 * 60 * 60)
     );
+    if (cacheFreshness < minCacheInterval) {
+        if (!config.forceCache) {
+            console.log(`Cache is fresh now. Last cache saved less then ${cacheFreshness}h ago`);
+            return;
+        } else {
+            console.log(`Run force cache. Last cache saved less then ${cacheFreshness}h ago`);
+        }
+    }
+
+    const classesFilePath = path.resolve(__dirname, "../public/data/classes.json");
+
+    await promisifiedWriteFile(classesFilePath, JSON.stringify(ClassesWithSpecs));
+
     const classSpecPairs = getClassSpecPairs();
 
     await pMap(
@@ -32,10 +51,7 @@ async function run() {
         { concurrency: 1 },
     );
 
-    await promisifiedWriteFile(
-        path.resolve(__dirname, "../public/data/meta.json"),
-        JSON.stringify({ lastCache: Date.now() })
-    );
+    await promisifiedWriteFile(metaFilePath, JSON.stringify({ lastCache: Date.now() }));
 }
 
 run()
