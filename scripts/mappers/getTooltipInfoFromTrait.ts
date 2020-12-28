b39a84f3cdd2be7cc8ec4ce0f236608123b24952
Alex Popkov <poalrom@yandex.ru>
2020-12-28 23:36:29 +0300

    fix: fix error with tooltip cache

--- scripts/mappers/getTooltipInfoFromTrait.ts
+++ scripts/mappers/getTooltipInfoFromTrait.ts
@@ -2,7 +2,12 @@ import { IBnetAPI } from "../BnetAPI/BnetAPI";
 import { IConduit } from "../interfaces/IConduit";
 import { ITrait } from "../interfaces/ISoulbinds";
 
-const TRAIT_CACHE: Record<string, IConduit> = {};
+interface ITooltip {
+    id: number;
+    type: 'spell' | 'item';
+}
+
+const TRAIT_CACHE: Record<string, ITooltip> = {};
 
 export async function getTooltipInfoFromTrait(trait: ITrait, bnetAPI: IBnetAPI) {
     const href = trait.trait ? trait.trait.key.href : trait.conduit_socket.socket.conduit.key.href;
@@ -12,10 +17,10 @@ export async function getTooltipInfoFromTrait(trait: ITrait, bnetAPI: IBnetAPI) 
     }
 
     const traitInfo = await bnetAPI.fetchByURL<IConduit>(href);
-    TRAIT_CACHE[href] = traitInfo;
-
-    return {
+    TRAIT_CACHE[href] = {
         id: traitInfo.spell_tooltip ? traitInfo.spell_tooltip.spell.id : traitInfo.item.id,
         type: traitInfo.spell_tooltip ? 'spell' : 'item',
-    }
+    };
+
+    return TRAIT_CACHE[href];
 }
