cbec86ade5a6fdf752923870de94957c34501484
Alex Popkov <poalrom@yandex.ru>
2020-12-28 22:16:44 +0300

    feat: add trait cache to trait tooltip loader

--- scripts/mappers/getTooltipInfoFromTrait.ts
+++ scripts/mappers/getTooltipInfoFromTrait.ts
@@ -2,9 +2,17 @@ import { IBnetAPI } from "../BnetAPI/BnetAPI";
 import { IConduit } from "../interfaces/IConduit";
 import { ITrait } from "../interfaces/ISoulbinds";
 
+const TRAIT_CACHE: Record<string, IConduit> = {};
+
 export async function getTooltipInfoFromTrait(trait: ITrait, bnetAPI: IBnetAPI) {
     const href = trait.trait ? trait.trait.key.href : trait.conduit_socket.socket.conduit.key.href;
+
+    if (TRAIT_CACHE[href]) {
+        return TRAIT_CACHE[href];
+    }
+
     const traitInfo = await bnetAPI.fetchByURL<IConduit>(href);
+    TRAIT_CACHE[href] = traitInfo;
 
     return {
         id: traitInfo.spell_tooltip ? traitInfo.spell_tooltip.spell.id : traitInfo.item.id,
